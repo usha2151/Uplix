35b5d43105c495fde6b950872ba2abdb6ec7033b
Usha Singh <ushasing215105@gmail.com>
2024-06-23 09:54:08 -0400

    new upadtes

--- client/src/Component/Pages/Users/Addusers.jsx
+++ client/src/Component/Pages/Users/Addusers.jsx
@@ -23,12 +23,12 @@ function Addusers() {
                <label className="mb-2.5 block text-black dark:text-white">Email <span className="text-meta-1">*</span></label>
                <input type="email" name="email" placeholder="Enter your email address"  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-1 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary" />
              </div>
-           <button className="inline-flex justify-center items-center mt-8 rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm text-white font-medium bg-blue-500 h-8">Cancel</button>
+           <button className="inline-flex justify-center items-center mt-8 rounded-md border border-transparent bg-red py-2 px-4 text-sm text-white font-medium bg-blue-500 h-8">Cancel</button>
            </div>
            </div>
         
          <div className="mt-4 flex justify-end gap-2">
-           <button  className="inline-flex justify-center rounded-md border border-transparent bg-green py-2 px-4 text-sm text-white font-medium bg-blue-500"><i class="fa-solid fa-user-plus mr-1 mt-1"></i>Add Users</button>
+           <button  className="inline-flex justify-center rounded-md border border-transparent bg-blue py-2 px-4 text-sm text-white font-medium bg-blue-500"><i class="fa-solid fa-user-plus mr-1 mt-1"></i>Add Users</button>
          </div>
 
          <p className="text-center">OR</p>
